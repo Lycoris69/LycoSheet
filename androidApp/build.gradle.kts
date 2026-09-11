1a4ae50a57618e9634419b2e452bacbba9d2bd7a
Lycoris69 <simeon.petrou@epitech.eu>
2026-09-11 15:22:01 +0200

    fix: versionName 0.1.7 (was 0.1.6 in v0.1.7 APK)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.lycoris.lycosheet.android"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 6
-        versionName = "0.1.6"
+        versionCode = 7
+        versionName = "0.1.7"
     }
 
     signingConfigs {
